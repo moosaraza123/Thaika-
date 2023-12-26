483c84b976145b2557b15e3b011dae37937fc9f4
moosa <razamoosa538@gmail.com>
2023-12-26 21:52:33 +0500

    routes creations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/about'
+import SignIn from './pages/signin'
+import SignUp from './pages/signUp'
+import Profile from './pages/Profile'
+import HousePrediction from './pages/HousePrediction'
 import React from 'react'
 import './app.css'
 function App() {
-  return (
-    <div>
-      <h1 className='text'>App</h1>
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+<Route path='/' element={<Home/>}></Route>
+<Route path='/sign-in' element={<SignIn/>}></Route>
+<Route path='/sign-up' element={<SignUp/>}></Route>
+<Route path='/about' element={<About/>}></Route>
+<Route path='/profile' element={<Profile/>}></Route>
+<Route path='/price-predict' element={<HousePrediction/>}></Route>
+
+
+
+  </Routes>
+  
+  
+  
+  </BrowserRouter>
 }
 
 export default App
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+      signOut 
+    </div>
+  )
+}
+
+export default SignUp

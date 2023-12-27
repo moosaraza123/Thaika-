c24c4b098f1a5999cd56ae40079e9bd072966a2a
moosa <razamoosa538@gmail.com>
2023-12-27 21:07:51 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{console.log('success')}).catch(()=>{
+    console.log('eoooooo')
+})
 const app=express();
 app.listen(3000,()=>{
     console.log('server running on port 3000')

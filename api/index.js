9dfba996b4bab79b6bfe4998aae13401b5d64e2d
moosa <razamoosa538@gmail.com>
2023-12-27 23:10:48 +0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
+import express from 'express';
+import userRouter from './routes/user.route.js';
+import AuthRouter from './routes/auth.route.js';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{console.log('success')}).catch(()=>{
     console.log('eoooooo')
 })
 const app=express();
+app.use(express.json())
 app.listen(3000,()=>{
     console.log('server running on port 3000')
 })
+
+app.use("/api/user",userRouter)
+app.use("/api/auth",AuthRouter)
---
+++ api/routes/auth.route.js
@@ -0,0 +1,5 @@
+import express from 'express'
+import { signup } from '../controllers/auth.controller.js'
+const AuthRouter=express.Router()
+AuthRouter.post("/signup",signup)
+export default AuthRouter;
